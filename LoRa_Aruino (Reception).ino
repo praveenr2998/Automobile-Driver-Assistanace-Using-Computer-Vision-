a0c0ce72357476e5de5c033cac0c0790d2b79c1f
praveenr2998 <51623786+praveenr2998@users.noreply.github.com>
2019-07-24 08:15:19 +0530

    Add files via upload

---
+++ LoRa_Aruino (Reception).ino
@@ -0,0 +1,49 @@
+#include <SPI.h>
+#include <LoRa.h>
+
+#define in1 2
+#define in2 3
+
+void setup() {
+  Serial.begin(9600);
+      
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+   digitalWrite(in1, HIGH);
+   digitalWrite(in2, LOW);
+     
+  while (!Serial);
+
+  Serial.println("LoRa Receiver");
+
+  if (!LoRa.begin(915E6)) {
+    Serial.println("Starting LoRa failed!");
+    while (1);
+  }
+}
+
+void loop() {
+  
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  
+  // try to parse packet
+  int packetSize = LoRa.parsePacket();
+  if (packetSize) {
+    // received a packet
+    Serial.print("Received packet '");
+
+    // read packet
+    while (LoRa.available()) {
+      Serial.print((char)LoRa.read());
+      
+      digitalWrite(in1, HIGH);
+      digitalWrite(in2, HIGH);
+      delay(5000);
+    }
+
+    // print RSSI of packet
+    Serial.print("' with RSSI ");
+    Serial.println(LoRa.packetRssi());
+  }
+}

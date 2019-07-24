ad9927cf3d396b6d01f956cb1e21ae679aabf184
praveenr2998 <51623786+praveenr2998@users.noreply.github.com>
2019-07-24 07:52:58 +0530

    Add files via upload
    Python Code to detect pedestrians using HOG descriptor.

---
+++ web_detect.py
@@ -0,0 +1,54 @@
+# USAGE
+# python detect.py --images images
+
+# import the necessary packages
+from __future__ import print_function
+from imutils.object_detection import non_max_suppression
+from imutils import paths
+import numpy as np
+import imutils
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+# loop over the image paths
+
+
+while True:
+	# load the image and resize it to (1) reduce detection time
+	# and (2) improve detection accuracy
+	ret,image = cap.read()
+	#image = imutils.resize(image, width=min(400, image.shape[1]))
+	orig = image.copy()
+
+	# detect people in the image
+	(rects, weights) = hog.detectMultiScale(image, winStride=(4, 4),
+		padding=(8, 8), scale=1.05)
+
+	# draw the original bounding boxes
+	for (x, y, w, h) in rects:
+		cv2.rectangle(orig, (x, y), (x + w, y + h), (0, 0, 255), 2)
+
+	# apply non-maxima suppression to the bounding boxes using a
+	# fairly large overlap threshold to try to maintain overlapping
+	# boxes that are still people
+	rects = np.array([[x, y, x + w, y + h] for (x, y, w, h) in rects])
+	pick = non_max_suppression(rects, probs=None, overlapThresh=0.65)
+
+	# draw the final bounding boxes
+	for (xA, yA, xB, yB) in pick:
+		cv2.rectangle(image, (xA, yA), (xB, yB), (0, 255, 0), 2)
+
+
+	# show the output images
+	cv2.imshow("After NMS", image)
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
+
+# When everything is done, release the capture
+cap.release()
+cv2.destroyAllWindows()
